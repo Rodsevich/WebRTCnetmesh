4a96fe3c7a32aebf479cd34a6a479431e2efa0a8
Rodsevich <neton_12@hotmail.com>
2016-11-04 17:05:03 -0300

    sorp

--- test/integration/cliente_test.dart
+++ test/integration/cliente_test.dart
@@ -1,26 +1,62 @@
 @TestOn("browser")
 import "dart:html";
 // import "dart:async";
+import 'package:WebRTCnetmesh/src/WebRTCnetmesh_base.dart';
 import "package:test/test.dart";
 import "package:WebRTCnetmesh/WebRTCnetmesh_client.dart";
-import 'package:WebRTCnetmesh/src/Identidad.dart';
+
+class Imprimir extends CommandImplementation {
+  @override
+  askForPermission() {
+    return true;
+  }
+
+  @override
+  Impresor executor;
+
+  Imprimir(Impresor imp) {
+    this.executor = imp;
+  }
+
+  @override
+  execute() {
+    this.executor.agregarMsj(this.arguments["valor"]);
+  }
+}
+
+class Impresor {
+  Impresor(this.actual);
+  List<String> buffer = [];
+  String actual;
+  agregarMsj(String msj) {
+    buffer.add(actual);
+    actual = msj;
+  }
+}
 
 void main() {
   print("test cliente");
-  Identidad id1 = new Identidad("cliUno");
-  Identidad id2 = new Identidad("cliDos");
+
+  Impresor imp1 = new Impresor("actual");
+  Impresor imp2 = new Impresor("actual");
+
+  Imprimir cmdImp = new Imprimir(imp1);
+  Imprimir cmdImp = new Imprimir(imp1);
+
+  Identity id1 = new Identity("cliUno");
+  Identity id2 = new Identity("cliDos");
 
   group("Inicios de sesion", () {
     test("Conexion cliente 1", () async {
       WebRTCnetmesh cliente1 = new WebRTCnetmesh(id1);
-      cliente1.onNewConnection.listen(expectAsync((Identidad id) {
-        expect(id.nombre, equals("cliUno"));
+      cliente1.onNewConnection.listen(expectAsync((Identity id) {
+        expect(id.name, equals("cliUno"));
       }));
     });
     test("2 conexiones", () async {
-      WebRTCnetmesh cliente2 = new WebRTCnetmesh(id2);
-      cliente2.onNewConnection.listen(expectAsync((Identidad id) {
-        expect(id.nombre, equals("cliDos"));
+      WebRTCnetmesh cliente2 = new WebRTCnetmesh(identity, commandImplementations)sh(id2);
+      cliente2.onNewConnection.listen(expectAsync((Identity id) {
+        expect(id.name, equals("cliDos"));
       }));
     });
   });
